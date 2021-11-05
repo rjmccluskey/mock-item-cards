f5cb792a34e2c2546603d0b7965e3fd1b3144ca9
Rich McCluskey <rjmccluskey@gmail.com>
2021-11-04 22:43:27 -0700

    Wire up getItem API to ItemPage

--- src/components/ItemPage.tsx
+++ src/components/ItemPage.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import { getItem, Item } from "../services/items";
 
 export const ItemPage: React.FC = () => {
-  return <p>Item Page!</p>;
+  const { id } = useParams();
+  const [item, setItem] = useState<Item>();
+  useEffect(() => {
+    if (id) {
+      getItem(id).then(setItem);
+    }
+  }, [id]);
+
+  return (
+    <>
+      {item && <p>{item.name}</p>}
+    </>
+  );
 };
