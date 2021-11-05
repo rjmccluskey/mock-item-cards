8730e095a270b0ca73e0205cdb5724f66a86ed8d
Rich McCluskey <rjmccluskey@gmail.com>
2021-11-04 21:28:35 -0700

    Truncate item card variants list to 4 max

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { WrapItem, Heading, List, ListItem, Button } from "@chakra-ui/react";
-import { Item } from "../services/items";
+import { Item, Variant } from "../services/items";
 
-export interface ItemCardProps {
-  item: Item;
+const Variants: React.FC<{variants: Variant[]}> = ({ variants }) => {
+  const firstFour = variants.slice(0, 4);
+  const remainder = variants.length - firstFour.length;
+  return (
+    <List>
+      {firstFour.map((variant, i) =>
+        <ListItem key={variant.id}>
+          Item {i + 1} - {variant.name}
+        </ListItem>)}
+      {remainder ? <ListItem>... plus {remainder} more</ListItem> : null}
+    </List>
+  );
 }
 
-export const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
+export const ItemCard: React.FC<{item: Item}> = ({ item }) => {
   return (
     <WrapItem w="240px" h="240px" borderWidth="1px" borderRadius="lg" overflow="hidden" flexDirection="column">
       <Heading as="h5" size="sm">{item.name}</Heading>
-      <List>
-        {item.variants.map((variant, i) =>
-          <ListItem key={variant.id}>
-            Item {i + 1} - {variant.name}
-          </ListItem>)}
-      </List>
+      <Variants variants={item.variants} />
       <Button mt="auto" alignSelf="center">SELECT</Button>
     </WrapItem>
   );
