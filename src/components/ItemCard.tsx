deb3457d4562edef2c440d9892dcad6404346327
Rich McCluskey <rjmccluskey@gmail.com>
2021-11-04 21:14:21 -0700

    Implement basic parts of the ItemCard
    This adds the item name, list of variants, and a select button. More styling and
    functionality will come later, but this is a good starting point.

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { WrapItem, Heading, List, ListItem, Button } from "@chakra-ui/react";
+import { Item } from "../services/items";
 
-export const ItemCard: React.FC = () => {
-  return null;
+export interface ItemCardProps {
+  item: Item;
+}
+
+export const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
+  return (
+    <WrapItem w="240px" h="240px" borderWidth="1px" borderRadius="lg" overflow="hidden" flexDirection="column">
+      <Heading as="h5" size="sm">{item.name}</Heading>
+      <List>
+        {item.variants.map((variant, i) =>
+          <ListItem key={variant.id}>
+            Item {i + 1} - {variant.name}
+          </ListItem>)}
+      </List>
+      <Button mt="auto" alignSelf="center">SELECT</Button>
+    </WrapItem>
+  );
 };
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getItems, Item } from "../services/items";
+import { Container, Wrap } from "@chakra-ui/react";
+import { ItemCard } from "./ItemCard";
 
 export const ItemList: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
@@ -8,8 +10,10 @@ export const ItemList: React.FC = () => {
   }, []);
 
   return (
-    <ul>
-      {items.map(item => <li>{item.name}</li>)}
-    </ul>
+    <Container maxW="container.xl">
+      <Wrap spacing="30px" justify="center">
+        {items.map(item => <ItemCard item={item} key={item.id} />)}
+      </Wrap>
+    </Container>
   );
 };
